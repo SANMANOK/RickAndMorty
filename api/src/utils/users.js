1bac38a05cbbd2db9eae80c53cdf3656f49f3e49
SANMANOK <santiagojoseortega301@gmail.com>
2023-06-23 13:40:57 -0300

    commit M3

---
+++ api/src/utils/users.js
@@ -0,0 +1,1 @@
+module.exports = [{email: "sanman@gmail.com", password: "sanmanok"}];
---
+++ client/src/views/about/about.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h1>Gama es un instructor muy simpatico y sus coleguillas lo quieren</h1>
+    </div>
+  );
+}
+
+export default About;
